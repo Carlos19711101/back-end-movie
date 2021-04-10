9e62f52757f8cfa0bb1cbe1e56e3b4501af89018
carlos velez <carlos.velezqest.iudigital.edu.co>
2021-04-10 12:08:26 -0500

    mi primer commit del back-end-movie

---
+++ src/main/java/co/edu/iudigital/app/FilmsSpringbootApp3Application.java
@@ -0,0 +1,13 @@
+package co.edu.iudigital.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmsSpringbootApp3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmsSpringbootApp3Application.class, args);
+	}
+
+}
